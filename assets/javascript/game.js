a99edfde0f5d9c89a5c7c72df52b086ea4edd4d8
Aja Strommen <9466667+astrommen@users.noreply.github.com>
2020-01-16 11:58:10 -0500

    added vars & random # generator

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,42 @@
 
+$(document).ready(function() {
+    var blue;
+    var green;
+    var yellow;
+    var red;
+    var crystalValue;
+    var userScore = 0;
+    var wins = 0;
+    var lose = 0;
+    
+    var crystalValueText = document.getElementById("crystal-value");
+    var userScoreText = document.getElementById("user-score");
+    var winScoreText = document.getElementById("wins");
+    var loseScoreText = document.getElementById("loses");
+    
+    //clicking crystals something happens
+    $(".crystals").on("click", function() {
+      alert("I've been clicked!");
+    });
+    
+    function initializeCollector() {
+      var userScore = 0;
+      
+      $("#user-score").empty();
+      
+      crystalValue = Math.floor(Math.random() * 101);console.log(crystalValue);
+      
+      blue = randomCrysVal();console.log("this is " + blue);
+      green = randomCrysVal();console.log("this is " + green);
+      yellow = randomCrysVal();console.log("this is " + yellow);
+      red = randomCrysVal();console.log("this is " + red);
+      
+    }
+    
+    function randomCrysVal() {
+      return Math.floor(Math.random() * 12);
+      
+    } 
+    initializeCollector();
+  });
+  
