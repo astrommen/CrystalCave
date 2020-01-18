d961c1d739f32d9c2596c4fdeae4dd41e0b09750
Aja Strommen <9466667+astrommen@users.noreply.github.com>
2020-01-18 10:15:53 -0500

    cleanup + total user score works

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,10 @@
 
 $(document).ready(function() {
-    var blue;
-    var green;
-    var yellow;
-    var red;
+
+    //Persistent/Global variables
     var crystalValue = 0;
-    var userScore = [];
+    var userGemScore = [];
+    var userScore = 0;
     var wins = 0;
     var lose = 0;
     
@@ -14,37 +13,55 @@ $(document).ready(function() {
     // var winScoreText = document.getElementById("wins");
     // var loseScoreText = document.getElementById("loses");
     
-    //clicking crystals something happens
-    $(".crystals").on("click", function() {
-      alert("I've been clicked!");
-    });
+    
+    function randomCrysVal() {
+      return Math.floor((Math.random() * 12) + 1); 
+    } 
     
     function initializeCollector() {
-      var userScore = [];
+      var userScore = 0;
       
       $("#user-score").empty();
       
-      crystalValue = Math.floor(Math.random() * 101);console.log(crystalValue);
+      crystalValue = Math.floor((Math.random() * 101) + 19);console.log(crystalValue);
       $(".crystal-value").text("Crystal Value = " + crystalValue);
       
-      $("#blue").attr("value", randomCrysVal()); console.log($(this).attr("value"));
-      $("#green").attr("value", randomCrysVal()); console.log($(this).attr("value"));
-      $("#yellow").attr("value", randomCrysVal()); console.log($(this).attr("value")); 
-      $("#red").attr("value", randomCrysVal()); console.log($(this).attr("value"));
+      // $("#blue").attr("value", randomCrysVal()); 
+      // $("#green").attr("value", randomCrysVal()); 
+      // $("#yellow").attr("value", randomCrysVal()); 
+      // $("#red").attr("value", randomCrysVal()); 
       
+      addIdValue("#blue");
+      addIdValue("#green");
+      addIdValue("#yellow");
+      addIdValue("#red");
     }
     
-    function randomCrysVal() {
-      return Math.floor(Math.random() * 12); 
-    } 
+    function addIdValue(id){
+      $(id).attr("value", randomCrysVal()); 
+    }
 
     //Game Play
     $(".crystals").on("click", function(){
-    
-    userScore.push($(this).attr("value")); console.log(userScore);
-    
-    
+  
+      userGemScore.push($(this).attr("value")); console.log(userGemScore);
+
+      userScore = userGemScore.reduce(function(a, b){//reduce() method is an array method only!
+          return Number(a) + Number(b);
+      }); console.log(userScore);
+
+      // $(".user-score").text(userScore);
+      
     });
+
+    //Win/Lose Condition
+    if (userScore == crystalValue){
+      var w = 0;
+      w++;
+    } else if (userScore > crystalValue){
+      var l = 0;
+      l++;
+    }
     
     initializeCollector();
   });
